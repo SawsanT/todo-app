338d2083599bc1c4f051464a98086c7302198e4e
SawsanT <aljuhanisawsan@gmail.com>
2023-09-30 21:00:49 +0300

    First commit

---
+++ tempData.js
@@ -0,0 +1,47 @@
+export default data = [
+    {
+        name: "Work",
+        color: "#6C3428",
+        todos:[
+            {
+                title: "End up the Database Course",
+                completed: true
+            }, 
+            {
+                title: "Apply changes on the website",
+                completed: true
+            }, 
+            {
+                title: "End up the Database Course",
+                completed: true
+            }, 
+            {
+                title: "Apply changes on the website",
+                completed: false
+            }
+        ]
+    }, 
+
+    {
+        name: "Home",
+        color: "#D85963",
+        todos:[
+            {
+                title: "End up the Database Course",
+                completed: true
+            }, 
+            {
+                title: "Apply changes on the website",
+                completed: false
+            }, 
+            {
+                title: "End up the Database Course",
+                completed: true
+            }, 
+            {
+                title: "Apply changes on the website",
+                completed: false
+            }
+        ]
+    }
+]
